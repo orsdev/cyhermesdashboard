5686a0da1e819f571ed7b4612bef628c94b128fa
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-12-22 11:36:39 +0100

    responsiveness issues fixed

--- src/components/main.js
+++ src/components/main.js
@@ -1,4 +1,5 @@
 import { Layout } from 'antd';
+import Header from './header';
 import AddUsers from './addUsers';
 import Modules from './modules';
 import Buttons from './buttons';
@@ -9,27 +10,9 @@ function Main() {
     <main className="main">
       <Layout
         style={{
-          padding: 0,
-          paddingLeft: 80,
-          paddingRight: 80,
           background: 'white',
         }}>
-        <header className="main-header">
-          <div className="main-header-info">
-            <h5 className="main-header-info-name">HEY, TERRY</h5>
-            <h2 className="main-header-info-text">
-              Start your <em>30 days</em> Trial
-            </h2>
-          </div>
-          <div className="main-header-balance">
-            <h5 className="main-header-balance-sign">NGN</h5>
-            <h2 className="main-header-balance-price">1,999,999</h2>
-            <p className="main-header-balance-footer">
-              Total
-              <span> (EXCLUDES VAT)</span>
-            </p>
-          </div>
-        </header>
+        <Header />
         <Plan />
         <AddUsers />
         <Modules />
